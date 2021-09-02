0090b737231366b956a7f1ae863892735fd9153b
Zarxong <89886162+Zarxong@users.noreply.github.com>
2021-09-02 19:04:58 -0400

    Update AddContact.php

--- php/AddContact.php
+++ php/AddContact.php
@@ -15,7 +15,7 @@
 
     # establish connection to MySQL server to access database and handle failed
     # connection error case
-    $conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName); #Might have to change this idk tho
+    $conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName); 
     if( $conn->connect_error )
     {
 		returnWithError( $conn->connect_error );
@@ -25,39 +25,25 @@
 	# validation constraints are met or else return an error
     else
     {
-		$stmt = $conn->prepare("INSERT into Contacts ( FirstName, LastName, Details, PhoneNumber, Email) VALUES ( ?, ?, ?, ?, ?)");
+		
+	    
+		$stmt = $conn->prepare("INSERT INTO Contacts ( FirstName, LastName, Details, PhoneNumber, Email) VALUES ( ?, ?, ?, ?, ?)");
 		$stmt->bind_param("sssss",  $FirstName, $LastName, $Details, $PhoneNumber, $Email);
 		$stmt->execute();
-		$result = $stmt->get_result();
-		returnWithError("");
+		#$result = $stmt->get_result();
+		#returnWithError("");
 	    	
-	    	if($row = $result->fetch_assoc() ){
+	    	#if($row = $result->fetch_assoc() ){
 		
-			returnWithInfo($row['FirstName'], $row['LastName'], $row['Details'], $row['PhoneNumber'], $row['Email']);
+		#	returnWithInfo($row['FirstName'], $row['LastName'], $row['Details'], $row['PhoneNumber'], $row['Email']);
 			
-		}
-	    else {
-		returnWithError("Something went wrong");    	
-	    }
+		#}
+	    #else {
+		#returnWithError("Something went wrong");    	
+	    #}
 	    	$stmt->close();
 		$conn->close();
 	}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
 	
 ?>

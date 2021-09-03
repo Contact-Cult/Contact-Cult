4b364819d993bb0c53a910a55597a91d8d5f0dff
Zarxong <89886162+Zarxong@users.noreply.github.com>
2021-09-02 20:21:03 -0400

    Update test.php

--- php/test.php
+++ php/test.php
@@ -1,53 +1,39 @@
 <?php
-    require_once 'DBHandler.php';
-    require_once 'functions.php';
+	$inData = getRequestInfo();
+	
+	$color = $inData["color"];
+	$userId = $inData["userId"];
 
-    $inData = getRequestInfo();
-
-    # Contact Book User registration information stored as variables.
-    #$DateCreated = date("Y/m/d"); #Might not need
-    $FirstName = $inData["FirstName"];
-    $LastName = $inData["LastName"];
-    $Details = $inData["Contact Details"];
-    $PhoneNumber = $inData["PhoneNumber"];
-    $Email = $inData["Email"];
-
-
-    # establish connection to MySQL server to access database and handle failed
-    # connection error case
-    $conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName); 
-    if( $conn->connect_error )
-    {
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
 		returnWithError( $conn->connect_error );
-    }
-
-    # Query the database to insert the registered user into the Users table if
-	# validation constraints are met or else return an error
-    else
-    {
-		
-	    
-		$stmt = $conn->prepare("INSERT INTO Contacts ( FirstName, LastName, Details, PhoneNumber, Email) VALUES ( ?, ?, ?, ?, ?)");
-		$stmt->bind_param("sssss",  $FirstName, $LastName, $Details, $PhoneNumber, $Email);
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
+		$stmt->bind_param("ss", $userId, $color);
 		$stmt->execute();
-		#$result = $stmt->get_result();
-		#returnWithError("");
-	    	
-	    	#if($row = $result->fetch_assoc() ){
-		
-		#	returnWithInfo($row['FirstName'], $row['LastName'], $row['Details'], $row['PhoneNumber'], $row['Email']);
-			
-		#}
-	    #else {
-		#returnWithError("Something went wrong");    	
-	    #}
-	    	$stmt->close();
+		$stmt->close();
 		$conn->close();
+		returnWithError("");
 	}
 
-	function returnWithInfo( $FirstName, $LastName, $ID )
-		{
-			$retValue = '{"ID":' . $ID . ',"FirstName":"' . $FirstName . '","LastName":"' . $LastName . '","error":""}';
-			sendResultInfoAsJson( $retValue );
-		}
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
 ?>

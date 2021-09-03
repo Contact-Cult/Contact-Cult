b13eb2c94d6ee98a4fdcc2056b275522b4e6f375
rbugge <ryan.bugge@tuta.io>
2021-09-02 20:33:32 -0400

    php test

--- php/AddContact.php
+++ php/AddContact.php
@@ -30,11 +30,8 @@ require_once 'functions.php';
 $inData = getRequestInfo();
 
 # Contact Book User registration information stored as variables.
-$DateCreated = date("Y/m/d");
-$FirstName = $inData["FirstName"];
-$LastName = $inData["LastName"];
-$UserName = $inData["UserName"];
-$Password = $inData["Password"];
+    $color = $inData["color"];
+    $userId = $inData["userId"];
 
 
 # establish connection to MySQL server to access database and handle failed
@@ -49,24 +46,24 @@ if( $conn->connect_error )
 # validation constraints are met or else return an error
 else
 {
-    $stmt = $conn->prepare("SELECT * FROM Users WHERE UserName =?");
-    $stmt->bind_param("s", $UserName);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $row = $result->fetch_assoc();
-    $stmt->close();
+    #$stmt = $conn->prepare("SELECT * FROM Users WHERE UserName =?");
+    #$stmt->bind_param("s", $UserName);
+    #$stmt->execute();
+    #$result = $stmt->get_result();
+    #$row = $result->fetch_assoc();
+    #$stmt->close();
 
-    if ($UserName == $row["UserName"])
+    if (true)
     {
         returnWithError("Username already exists.");
     }
     else
     {
-        $stmt = $conn->prepare("INSERT INTO Users (DateCreated, FirstName, LastName, UserName, Password) VALUES (?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssss", $DateCreated, $FirstName, $LastName, $UserName, $Password);
-        $stmt->execute();
-        $stmt->close();
-        $conn->close();
+        #$stmt = $conn->prepare("INSERT INTO Users (DateCreated, FirstName, LastName, UserName, Password) VALUES (?, ?, ?, ?, ?)");
+        #$stmt->bind_param("sssss", $DateCreated, $FirstName, $LastName, $UserName, $Password);
+        #$stmt->execute();
+        #$stmt->close();
+        #$conn->close();
         returnWithError("");
     }
 }

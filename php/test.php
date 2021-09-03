c581d5a8f70aabcd21f562037887267518ad8a3d
Zarxong <89886162+Zarxong@users.noreply.github.com>
2021-09-02 20:27:48 -0400

    Update test.php

--- php/test.php
+++ php/test.php
@@ -22,21 +22,6 @@
 		returnWithError("");
 	}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
 	
 ?>

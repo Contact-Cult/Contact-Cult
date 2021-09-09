7728a36c34cb8766d64d908de45a4f18bc0b7d57
Zarxong <89886162+Zarxong@users.noreply.github.com>
2021-09-08 22:21:35 -0400

    Update AddContact.php

--- php/AddContact.php
+++ php/AddContact.php
@@ -28,7 +28,7 @@
 	    $stmt->execute();
 	    
 $sid = $stmt->insert_id;
-	    printf("Last inserted record has id %d\n", mysql_insert_id());
+	    returnWithError($sid);
 
 	    #$stmt = $conn->prepare( SELECT SCOPE_IDENTITY);
 	    #$stmt->bind_param("sssssssss", $inData["ID"], $inData["FirstName"], $inData["LastName"],$inData["Address"],$inData["City"],$inData["State"],$inData["ZipCode"],$inData["PhoneNumber"],$inData["Email"]);

f8116b28e4b603b4a702ef3975a50b342a76ac9a
Zarxong <89886162+Zarxong@users.noreply.github.com>
2021-09-08 22:41:48 -0400

    Update SearchTest.php

--- php/SearchTest.php
+++ php/SearchTest.php
@@ -1,64 +1,53 @@
 <?php
+	require_once 'DBHandler.php';
+	require_once 'functions.php';
 
 	$inData = getRequestInfo();
 	
 	$searchResults = "";
 	$searchCount = 0;
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
-		$stmt = $conn->prepare("select Name from Colors where Name like ? and UserID=?");
-		$colorName = "%" . $inData["search"] . "%";
-		$stmt->bind_param("ss", $colorName, $inData["userId"]);
-		$stmt->execute();
 		
-		$result = $stmt->get_result();
 		
-		while($row = $result->fetch_assoc())
-		{
-			if( $searchCount > 0 )
-			{
-				$searchResults .= ",";
-			}
-			$searchCount++;
-			$searchResults .= '"' . $row["Name"] . '"';
-		}
+		SELECT * FROM Contacts WHERE CONCAT(FirstName,  ' ', LastName) LIKE  '%B%' OR CONCAT(LastName,  ' ', FirstName) LIKE '%B%';
+
+		#$stmt = $conn->prepare("select * from Contacts where FirstName like ? or LastName and UserID=?");
+		#$colorName = "%" . $inData["search"] . "%";
+		#$stmt->bind_param("ss", $colorName, $inData["userId"]);
+		#$stmt->execute();
 		
-		if( $searchCount == 0 )
-		{
-			returnWithError( "No Records Found" );
-		}
-		else
-		{
-			returnWithInfo( $searchResults );
-		}
+		#$result = $stmt->get_result();
+		
+		#while($row = $result->fetch_assoc())
+		#{
+		#	if( $searchCount > 0 )
+		#	{
+		#		$searchResults .= ",";
+		#	}
+		#	$searchCount++;
+		#	$searchResults .= '"' . $row["Name"] . '"';
+		#}
+		
+		#if( $searchCount == 0 )
+		#{
+		#	returnWithError( "No Records Found" );
+		#}
+		#else
+		#{
+		#	returnWithInfo( $searchResults );
+		#}
 		
 		$stmt->close();
 		$conn->close();
 	}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
 	function returnWithInfo( $searchResults )
 	{
 		$retValue = '{"results":[' . $searchResults . '],"error":""}';

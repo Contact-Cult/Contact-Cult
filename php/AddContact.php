8a6210ef47f4c197cc6df27784ab14b1c05b6461
Zarxong <89886162+Zarxong@users.noreply.github.com>
2021-09-08 22:13:23 -0400

    Update AddContact.php

--- php/AddContact.php
+++ php/AddContact.php
@@ -28,8 +28,8 @@
 	    $stmt->execute();
 	    
 
-	    #SELECT LAST_INSERT_ID();
-  
+	    INSERT INTO test (col1) VALUES ('val1');
+SELECT LAST_INSERT_ID();
 	    #$stmt = $conn->prepare( SELECT SCOPE_IDENTITY);
 	    #$stmt->bind_param("sssssssss", $inData["ID"], $inData["FirstName"], $inData["LastName"],$inData["Address"],$inData["City"],$inData["State"],$inData["ZipCode"],$inData["PhoneNumber"],$inData["Email"]);
 	    #$stmt->execute();

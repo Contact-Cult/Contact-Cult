39dcbf4b2420b2111b1b91e6e47e240c272cc015
Zarxong <89886162+Zarxong@users.noreply.github.com>
2021-09-08 22:06:18 -0400

    Update AddContact.php

--- php/AddContact.php
+++ php/AddContact.php
@@ -30,9 +30,7 @@
 
 	    
 	    SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY];  
-GO  
-SELECT @@IDENTITY AS [@@IDENTITY];  
-GO  
+  
 	    #$stmt = $conn->prepare( SELECT SCOPE_IDENTITY);
 	    #$stmt->bind_param("sssssssss", $inData["ID"], $inData["FirstName"], $inData["LastName"],$inData["Address"],$inData["City"],$inData["State"],$inData["ZipCode"],$inData["PhoneNumber"],$inData["Email"]);
 	    #$stmt->execute();

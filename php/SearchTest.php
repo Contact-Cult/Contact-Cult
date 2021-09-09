86d5dc6ed8cfa4f47bdc2066c538fddcd08ba538
Zarxong <89886162+Zarxong@users.noreply.github.com>
2021-09-09 01:21:07 -0400

    Update SearchTest.php

--- php/SearchTest.php
+++ php/SearchTest.php
@@ -1,59 +1,134 @@
 <?php
-	require_once 'DBHandler.php';
-	require_once 'functions.php';
+    require_once 'DBHandler.php';
+    require_once 'functions.php';
 
-	$inData = getRequestInfo();
-	
-	$ID = $inData["ID"];	
-	$searchResults = "";
-	$searchCount = 0;
+    $inData = getRequestInfo();
+    $searchResult = "";
+    $resultCount = 0;
+    $searchFilter = $inData["searchFilter"];
+    $searchQuery = $inData["searchQuery"];
 
-	$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		
-		#$query = "SELECT * FROM Contacts WHERE ID = ? AND FirstName LIKE '%" . $inData["FirstName"] . "%' ";
-		#$stmt = $conn->prepare($query);
-		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Contacts WHERE FirstName=? AND LastName =? AND ID = $ID");
-		$FirstName = "%" . $inData["search"] . "%";
-		#$LastName = "%" . $inData["search"] . "%";
-		$stmt->bind_param("ss", $inData["ID"], $FirstName);
-		$stmt->execute();
-		
-		$result = $stmt->get_result();
-		
-		while($row = $result->fetch_assoc())
-		{
-			if( $searchCount > 0 )
-			{
-				$searchResults .= ",";
-			}
-			$searchCount++;
-			$searchResults .= '"' . $row["FirstName"] . '"';
-		}
-		
-		if( $searchCount == 0 )
-		{
-			returnWithError( "No Records Found" );
-		}
-		else
-		{
-			returnWithInfo( $searchResults );
-		}
-		
-		$stmt->close();
-		$conn->close();
-	}
+    # contact information stored as variables
+    $ID = $inData["ID"];
+    $ContactID = "";
+    $FirstName = "";
+    $LastName = "";
+    $Address = "";
+    $City = "";
+    $State = "";
+    $ZipCode = "";
+    $PhoneNumber = "";
+    $Email = "";
+
+    # establish connection to MySQL server to access database and handle failed
+    # connection error case
+    $conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
+    if($conn->connect_error)
+    {
+        returnWithError($conn->connect_error);
+    }
+
+    # select the contact ID of the contact with the specified ID and first/last name,
+    # then using the contact ID, select all attributes of that contact and return
+    # that information or else return an error
+    else
+    {
+        $searchResult .= '"results" : [';
+
+        $query = "SELECT * FROM Contacts WHERE ID = $ID";
+        switch($searchFilter) {
+            case "All":
+                $query .=   "FirstName LIKE '%" . $searchQuery .
+                            "%' OR LastName LIKE '%" . $searchQuery .
+                            "%' OR Address LIKE '%" . $searchQuery .
+                            "%' OR City LIKE '%" . $searchQuery .
+                            "%' OR State LIKE '%" . $searchQuery .
+                            "%' OR ZipCode LIKE '%" . $searchQuery .
+                            "%' OR PhoneNumber LIKE '%" . $searchQuery .
+                            "%' OR Email LIKE '%" . $searchQuery . "%'";
+                break;
+            case "Name":
+                $query .=   "FirstName LIKE '%" . $searchQuery .
+                            "%' OR LastName LIKE '%" . $searchQuery . "%'";
+                break;
+            case "First":
+                $query .=   "FirstName LIKE '%" . $searchQuery . "%'";
+                break;
+            case "Last":
+                $query .=   "LastName LIKE '%" . $searchQuery . "%'";
+                break;
+            case "Address":
+                $query .=   "Address LIKE '%" . $searchQuery .
+                            "%' OR City LIKE '%" . $searchQuery .
+                            "%' OR State LIKE '%" . $searchQuery .
+                            "%' OR ZipCode LIKE '%" . $searchQuery . "%'";
+                break;
+            case "Phone":
+                $query .=   "PhoneNumber LIKE '%" . $searchQuery . "%'";
+                break;
+            case "Email":
+                $query .=   "Email LIKE '%" . $searchQuery . "%'";
+                break;
+        }
+
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if($row = $result->fetch_assoc())
+        {
+            $searchResult .= '{';
+            $searchResult .= '"ContactID" : "' . $row["ContactID"] . '", ';
+            $searchResult .= '"FirstName" : "' . $row["FirstName"] . '", ';
+            $searchResult .= '"LastName" : "' . $row["LastName"] . '", ';
+            $searchResult .= '"Address" : "' . $row["Address"] . '", ';
+            $searchResult .= '"City" : "' . $row["City"] . '", ';
+            $searchResult .= '"State" : "' . $row["State"] . '", ';
+            $searchResult .= '"ZipCode" : "' . $row["ZipCode"] . '", ';
+            $searchResult .= '"PhoneNumber" : "' . $row["PhoneNumber"] . '", ';
+            $searchResult .= '"Email" : "' . $row["Email"] .  '"';
+            $searchResult .= '}';
+            $resultCount++;
 
 
-	function returnWithInfo( $searchResults )
-	{
-		$retValue = '{"results":[' . $searchResults . '],"error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+            while($row = $result->fetch_assoc())
+            {
+                if($resultCount > 0)
+                {
+                    $searchResult .= ",";
+                }
+                $resultCount++;
+                $searchResult .= '{';
+                $searchResult .= '"ContactID" : "' . $row["ContactID"] . '", ';
+                $searchResult .= '"FirstName" : "' . $row["FirstName"] . '", ';
+                $searchResult .= '"LastName" : "' . $row["LastName"] . '", ';
+                $searchResult .= '"Address" : "' . $row["Address"] . '", ';
+                $searchResult .= '"City" : "' . $row["City"] . '", ';
+                $searchResult .= '"State" : "' . $row["State"] . '", ';
+                $searchResult .= '"ZipCode" : "' . $row["ZipCode"] . '", ';
+                $searchResult .= '"PhoneNumber" : "' . $row["PhoneNumber"] . '", ';
+                $searchResult .= '"Email" : "' . $row["Email"] .  '"';
+                $searchResult .= '}';
+            }
+            $searchResult .= ']';
+            returnWithInfo($searchResult);
+        }
+
+        else
+        {
+            returnWithError("No Results Match");
+        }
+        $stmt->close();
+
+        $conn->close();
+
+    }
+
+
+    # obtain the login information based on the input parameters and send information
+    # as JSON element.
+    function returnWithInfo($searchResult)
+    {
+        $retValue = '{'. $searchResult .', "error" : ""}';
+        sendResultInfoAsJson($retValue);
+    }
 ?>

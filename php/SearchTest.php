45534abbe00954ca8d6fe068d3f0f1ae26e5595d
Zarxong <89886162+Zarxong@users.noreply.github.com>
2021-09-09 00:21:02 -0400

    Update SearchTest.php

--- php/SearchTest.php
+++ php/SearchTest.php
@@ -15,7 +15,7 @@
 	else
 	{
 		
-		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Contacts WHERE FirstName=? AND LastName =?");
+		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Contacts WHERE FirstName=? OR LastName =?");
 		$FirstName = "%" . $inData["search"] . "%";
 		#$LastName = "%" . $inData["search"] . "%";
 		$stmt->bind_param("sss", $inData["userId"], $FirstName, $LastName);

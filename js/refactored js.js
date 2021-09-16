5641e99e9cb20721e7cb9be91f890fefbd801b29
rbugge <ryan.bugge@tuta.io>
2021-09-16 01:00:53 -0400

    test

--- js/refactored js.js
+++ js/refactored js.js
@@ -4,7 +4,7 @@ var userId = 0;
 let remember = true;
 
 let contact = /*html*/`
-<div id="new-contact" data-bs-index="" class="card border-0 h-100 g-0 mt-2 ms-2" data-bs-toggle="modal" data-bs-target="#contact-details" onclick="openDetails()">
+<div id="new-contact" data-bs-index="" class="card border-0 h-100 g-0 mt-2 ms-2" data-bs-toggle="modal" data-bs-target="#contact-details" onclick="addBlur()">
     <div class="container-fluid d-inline-flex" >
         <img id="new-contact-img" src="" class="img card-img d-inline-flex" alt="">
 

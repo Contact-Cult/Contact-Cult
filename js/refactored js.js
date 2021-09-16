3459d1ca6526eb0a725195cbb35dd28404ffe14a
rbugge <ryan.bugge@tuta.io>
2021-09-16 02:42:40 -0400

    test

--- js/refactored js.js
+++ js/refactored js.js
@@ -10,7 +10,7 @@ let contact = /*html*/`
 
         <div class="container-fluid">
             <div class="ms-2 mt-1 d-block">
-                <h5 class="card-title d-inline-block text-truncate" id="new-contact-name"></h5>
+                <span class="card-title d-inline-block text-truncate" id="new-contact-name" style="max-width: 100px font-size: 18px"></span>
 
                 <div class="card-text">
                         <div>

ba251e6e39d67de2249ee0d88f3423c2599faabf
rbugge <ryan.bugge@tuta.io>
2021-09-16 01:19:04 -0400

    test

--- js/refactored js.js
+++ js/refactored js.js
@@ -27,7 +27,7 @@ let contact = /*html*/`
 
         <div class="d-inline-flex mt-2 me-2" data-bs-dismiss="modal" data-bs-target="#contact-details">
             <div class="dropdown w-25">
-                <i class="bi-three-dots-vertical" id="card-menu" data-bs-toggle="dropdown" style="font-size: 24px"></i>
+                <i class="bi-three-dots-vertical" id="card-menu" data-bs-toggle="dropdown" style="font-size: 24px" onClick="cardMenu(event)"></i>
                 <ul class="dropdown-menu dropdown-menu-end">
                     <li><a class="dropdown-item" href="">Edit</a></li>
                     <li><a class="dropdown-item text-danger" href="">Delete</a></li>
@@ -38,13 +38,13 @@ let contact = /*html*/`
 </div>
 `;
 
-$("#card-menu").on("click", function(e) {
+function cardMenu(e) {
     $("#contact-details").addClass("stop-modal");
-    console.log(e);
+    console.log(e)
     e.preventDefault();
     e.stopPropagation();
     e.stopImmediatePropagation();
-})
+}
 
 $("#contact-details").on("show.bs.modal", function() {
     if($("#contact-details").hasClass("stop-modal")) {

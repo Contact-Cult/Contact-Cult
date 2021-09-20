1baf02ede7594c3861942bf1efbb9f90bb80b5a6
rbugge <ryan.bugge@tuta.io>
2021-09-19 23:37:11 -0400

    notes test

--- js/contactManager.js
+++ js/contactManager.js
@@ -70,7 +70,7 @@ function updateDetails() {
     );
 
     // $("#details-notes").empty();
-    $("#details-notes").val(currentCard.Notes);
+    $("#details-notes").text(currentCard.Notes);
 }
 
 // Create an object with values from selected form ("#edit" or "#add")
